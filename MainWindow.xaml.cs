58d0f1fd0fbdaa97632758992a7d86b04cf3d92d
Gene Suelo <gene.suelo@gmail.com>
2025-03-19 19:36:11 +0100

    Updaterad logik i MainWindow

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,19 +16,53 @@ namespace TodoAppWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private TodoList _todoList;
         public MainWindow()
         {
             InitializeComponent();
+            _todoList = new TodoList();
         }
 
+        /// <summary>
+        /// Lägger till en todo-item i listan
+        /// </summary>
+        /// <param name="sender">sender från mainwindow</param>
+        /// <param name="e">event</param>
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
-
+            string task = TaskTextBox.Text;
+            if (!string.IsNullOrEmpty(task))
+            {
+                _todoList.AddTask(task);
+                UpdateTaskList();
+                TaskTextBox.Clear();
+            }
+        }
+        /// <summary>
+        /// Uppdaterar listan med todo-items.
+        /// </summary>
+        private void UpdateTaskList()
+        {
+            TasksListBox.Items.Clear();
+            foreach (var task in _todoList.GetAllTasks()) 
+            {
+                TasksListBox.Items.Add(task);
+            }
         }
 
+        /// <summary>
+        /// Tar bort valt todo-item
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void RemoveButton_Click(object sender, RoutedEventArgs e)
         {
-
+            if (TasksListBox.SelectedIndex >= 0)
+            {
+                _todoList.RemoveTask(TasksListBox.SelectedIndex);
+                UpdateTaskList();
+            }
         }
     }
 }
